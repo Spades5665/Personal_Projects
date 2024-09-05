eb2b259557b8c92e0dea7a2980ed72382af820a8
Caden Wilfong <spadesbusiness5665@gmail.com>
2024-09-05 13:06:13 -0400

    Start of Daily Programs

---
+++ Daily Coding Problems/09-05-2024.py
@@ -0,0 +1,10 @@
+"""
+Problem: Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+Example: With a list of [10, 15, 3, 7] and a k value of 17, the program should return true with 10 and 7.
+"""
+
+s = [10, 15, 3, 7]
+k = 17
+
+s.sort()
+print(s)
