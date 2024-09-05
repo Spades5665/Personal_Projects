4efd45ea3f50122b0123d3770f60c35ae60c089e
Caden Wilfong <spadesbusiness5665@gmail.com>
2024-09-05 13:16:32 -0400

    Finish 09-05-2024

--- Daily Coding Problems/09-05-2024.py
+++ Daily Coding Problems/09-05-2024.py
@@ -6,5 +6,18 @@ Example: With a list of [10, 15, 3, 7] and a k value of 17, the program should r
 s = [10, 15, 3, 7]
 k = 17
 
-s.sort()
-print(s)
+def hasSum(s, k):
+    val = False
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i] + s[j] > k:
+            j -= 1
+        elif s[i] + s[j] < k:
+            i += 1
+        else:
+            val = True
+            break
+    return val
+        
+print(hasSum(s, k))
